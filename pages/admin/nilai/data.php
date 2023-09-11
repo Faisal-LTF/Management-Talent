2d850a9d6495cf2c68a54af8b6810c01936f50b8
azazel99 <ichalofficial@gmail.com>
2023-09-11 15:13:19 +0700

    new

--- pages/admin/nilai/data.php
+++ pages/admin/nilai/data.php
@@ -8,98 +8,117 @@ if (!isset($_SESSION['nama'])) {
 } else {
 
 ?>
-<script src="../assets/vendor/libs/sweetalert2/sweetalert2.js"></script>
+    <script src="../assets/vendor/libs/sweetalert2/sweetalert2.js"></script>
 
-<script>
-function confirmDelete(id) {
-    Swal.fire({
-        icon: 'warning',
-        title: 'Anda yakin ingin menghapus data?',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!',
-        customClass: {
-            confirmButton: 'btn btn-primary me-1',
-            cancelButton: 'btn btn-label-secondary'
-        },
-        buttonsStyling: false
-    }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.href = `?page=hapus_nilai&id=${id}`;
+    <script>
+        function confirmDelete(id) {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Anda yakin ingin menghapus data?',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!',
+                customClass: {
+                    confirmButton: 'btn btn-primary me-1',
+                    cancelButton: 'btn btn-label-secondary'
+                },
+                buttonsStyling: false
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = `?page=hapus_nilai&id=${id}`;
+                }
+            });
         }
-    });
-}
-</script>
+    </script>
 
 
-<div class="content-wrapper">
-    <div class="container-xxl flex-grow-1 container-p-y">
-        <div class="col-12 text-end mb-3">
-            <a href="?page=tambah_nilai" class="btn btn-primary">Tambah Data</a>
-        </div>
-        <div class="card">
-            <div class="card-datatable table-responsive pt-0">
-                <table id="example" class="table align-items-center">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama Pegawai</th>
-                            <th class="text-center">Nilai Akhir Assesment</th>
-                            <th class="text-center">Nilai Rekam Jejak</th>
-                            <th class="text-center">Nilai Potensial</th>
-                            <th class="text-center">Predikat Kinerja Pegawai</th>
-                            <th class="text-center">Presensi Pegawai</th>
-                            <th class="text-center">Nilai Kinerja</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
+    <div class="content-wrapper">
+        <div class="container-xxl flex-grow-1 container-p-y">
+            <div class="col-12 text-end mb-3">
+                <a href="?page=tambah_nilai" class="btn btn-primary">Tambah Data</a>
+            </div>
+            <div class="card">
+                <div class="card-datatable table-responsive pt-0">
+                    <table id="example" class="table align-items-center">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Pegawai</th>
+                                <th class="text-center">Nilai Akhir Assesment</th>
+                                <th class="text-center">Nilai Rekam Jejak</th>
+                                <th class="text-center">Nilai Potensial</th>
+                                <th class="text-center">Predikat Kinerja Pegawai</th>
+                                <th class="text-center">Presensi Pegawai</th>
+                                <th class="text-center">Nilai Kinerja</th>
+                                <th class="text-center">Box</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
                             $no = 1;
                             $query = mysqli_query($link, "SELECT * FROM nilai n join pegawai p on n.id_pegawai = p.id_pegawai");
                             $i = 1;
                             while ($row = $query->fetch_array()) {
+                                $boxName = "Box 1"; // Default: Box 1
+
+                                if ($row['np'] < 70 && $row['nk'] < 70) {
+                                    $boxName = "Box 1";
+                                } elseif ($row['np'] >= 70 && $row['np'] <= 90 && $row['nk'] < 70) {
+                                    $boxName = "Box 2";
+                                } elseif ($row['np'] >= 91 && $row['np'] <= 100 && $row['nk'] < 70) {
+                                    $boxName = "Box 3";
+                                } elseif ($row['np'] < 70 && $row['nk'] >= 70 && $row['nk'] <= 90) {
+                                    $boxName = "Box 4";
+                                } elseif ($row['np'] >= 70 && $row['np'] <= 90 && $row['nk'] >= 70 && $row['nk'] <= 90) {
+                                    $boxName = "Box 5";
+                                } elseif ($row['np'] >= 91 && $row['np'] <= 100 && $row['nk'] >= 70 && $row['nk'] <= 90) {
+                                    $boxName = "Box 6";
+                                } elseif ($row['np'] < 70 && $row['nk'] >= 91 && $row['nk'] <= 100) {
+                                    $boxName = "Box 7";
+                                } elseif ($row['np'] >= 71 && $row['np'] <= 90 && $row['nk'] >= 91 && $row['nk'] <= 100) {
+                                    $boxName = "Box 8";
+                                } elseif ($row['np'] >= 91 && $row['np'] <= 100 && $row['nk'] >= 91 && $row['nk'] <= 100) {
+                                    $boxName = "Box 9";
+                                }
+
                             ?>
-                        <tr>
-                            <td align="left"><?= $i++ ?></td>
-                            <td><?= $row['nama']; ?></td>
-                            <td align="center"><?= $row['naa']; ?></td>
-                            <td align="center"><?= $row['nrj']; ?></td>
-                            <td align="center"><?= $row['np']; ?></td>
-                            <td align="center"><?= $row['pkp']; ?></td>
-                            <td align="center"><?= $row['pp']; ?></td>
-                            <td align="center"><?= $row['nk']; ?></td>
-                            <td class="w-5">
-                                <div class=" mt-3">
-                                    <button type="button"
-                                        class="btn btn-primary dropdown-toggle border-radius-lg px-3 py-1 "
-                                        id="dropdownMenuButton" data-bs-toggle="dropdown">
-                                        <i class="fa fa-bars"></i>
-                                    </button>
-                                    <ul class="dropdown-menu shadow-lg mt-2  dropdown-menu-end px-2 py-2 me-sm-n4"
-                                        role="menu">
-                                        <li><a class="dropdown-item border-radius-md"
-                                                href="?page=edit_nilai&id=<?= $row[0]; ?>"><i class="fa fa-edit"></i>
-                                                Edit Data</a></li>
-                                        <li><a class="dropdown-item border-radius-md"
-                                                onclick="confirmDelete(<?= $row[0]; ?>);" href="#">
-                                                <i class="fa fa-trash"></i> Hapus
-                                            </a></li>
-                                    </ul>
-                                </div>
-                            </td>
-                        </tr>
-                        <?php
+                                <tr>
+                                    <td align="left"><?= $i++ ?></td>
+                                    <td><?= $row['nama']; ?></td>
+                                    <td align="center"><?= $row['naa']; ?></td>
+                                    <td align="center"><?= $row['nrj']; ?></td>
+                                    <td align="center"><?= $row['np']; ?></td>
+                                    <td align="center"><?= $row['pkp']; ?></td>
+                                    <td align="center"><?= $row['pp']; ?></td>
+                                    <td align="center"><?= $row['nk']; ?></td>
+                                    <td align="center"><?= $boxName; ?></td>
+                                    <td class="w-5">
+                                        <div class=" mt-3">
+                                            <button type="button" class="btn btn-primary dropdown-toggle border-radius-lg px-3 py-1 " id="dropdownMenuButton" data-bs-toggle="dropdown">
+                                                <i class="fa fa-bars"></i>
+                                            </button>
+                                            <ul class="dropdown-menu shadow-lg mt-2  dropdown-menu-end px-2 py-2 me-sm-n4" role="menu">
+                                                <li><a class="dropdown-item border-radius-md" href="?page=edit_nilai&id=<?= $row[0]; ?>"><i class="fa fa-edit"></i>
+                                                        Edit Data</a></li>
+                                                <li><a class="dropdown-item border-radius-md" onclick="confirmDelete(<?= $row[0]; ?>);" href="#">
+                                                        <i class="fa fa-trash"></i> Hapus
+                                                    </a></li>
+                                            </ul>
+                                        </div>
+                                    </td>
+                                </tr>
+                            <?php
                             }
                             ?>
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<!--/ DataTable with Buttons -->
+    <!--/ DataTable with Buttons -->
 
 
 
--- public/dashboard.php
+++ public/dashboard.php
@@ -8,91 +8,91 @@
         <!-- Card Border Shadow -->
         <div class="row">
             <div class="col-sm-6 col-lg-4 mb-4">
-                <div class="card card-border-shadow-danger">
+                <div class="card card-border-shadow-warning bg-warning text-white">
                     <div class="card-body">
                         <div class="d-flex align-items-center  mb-2 pb-1">
                             <div class="avatar me-2">
                                 <a href="?page=data_box7">
-                                    <span class="avatar-initial rounded bg-label-danger"><i
-                                            class="ti ti-user ti-md"></i></span>
-                                </a>
-                            </div>
-                            <h4 class="ms-1 mb-0 ">7</h4>
-                        </div>
-                        <p class="mb-1">Kinerja Di Atas Ekspektasi
-                        </p>
-                        <p class="mb-0">
-                            <span class="fw-medium me-1">Potensi</span>
-                            <small class="text-muted">Rendah</small>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-4 mb-4">
-                <div class="card card-border-shadow-warning">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center mb-2 pb-1">
-                            <div class="avatar me-2">
-                                <a href="?page=data_box8">
                                     <span class="avatar-initial rounded bg-label-warning"><i
                                             class="ti ti-user ti-md"></i></span>
                                 </a>
                             </div>
-                            <h4 class="ms-1 mb-0">8</h4>
+                            <h4 class="ms-1 mb-0 text-white">7</h4>
                         </div>
                         <p class="mb-1">Kinerja Di Atas Ekspektasi
                         </p>
                         <p class="mb-0">
                             <span class="fw-medium me-1">Potensi</span>
-                            <small class="text-muted">Menengah</small>
+                            <small class="">Rendah</small>
                         </p>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-lg-4 mb-4">
-                <div class="card card-border-shadow-info">
+                <div class="card card-border-shadow-success bg-success text-white">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center mb-2 pb-1">
+                            <div class="avatar me-2">
+                                <a href="?page=data_box8">
+                                    <span class="avatar-initial rounded bg-label-success"><i
+                                            class="ti ti-user ti-md"></i></span>
+                                </a>
+                            </div>
+                            <h4 class="ms-1 mb-0 text-white">8</h4>
+                        </div>
+                        <p class="mb-1">Kinerja Di Atas Ekspektasi
+                        </p>
+                        <p class="mb-0">
+                            <span class="fw-medium me-1">Potensi</span>
+                            <small class="">Menengah</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4 mb-4">
+                <div class="card card-border-shadow-success bg-success text-white">
                     <div class="card-body">
                         <div class="d-flex align-items-center mb-2 pb-1">
                             <div class="avatar me-2">
                                 <a href="?page=data_box9">
-                                    <span class="avatar-initial rounded bg-label-info"><i
+                                    <span class="avatar-initial rounded bg-label-success"><i
                                             class="ti ti-user ti-md"></i></span>
                                 </a>
                             </div>
-                            <h4 class="ms-1 mb-0">9</h4>
+                            <h4 class="ms-1 mb-0 text-white">9</h4>
                         </div>
                         <p class="mb-1">Kinerja Di Atas Ekspektasi
                         </p>
                         <p class="mb-0">
                             <span class="fw-medium me-1">Potensi</span>
-                            <small class="text-muted">Tinggi</small>
+                            <small class="">Tinggi</small>
                         </p>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-lg-4 mb-4">
-                <div class="card card-border-shadow-danger">
+                <div class="card text-white card-border-shadow-danger bg-danger">
                     <div class="card-body">
                         <div class="d-flex align-items-center mb-2 pb-1">
-                            <div class="avatar me-2">
+                            <div class="avatar  me-2">
                                 <a href="?page=data_box4">
                                     <span class="avatar-initial rounded bg-label-danger"><i
                                             class="ti ti-user ti-md"></i></span>
                             </div>
                             </a>
-                            <h4 class="ms-1 mb-0">4</h4>
+                            <h4 class="ms-1 mb-0 text-white">4</h4>
                         </div>
                         <p class="mb-1">Kinerja Sesuai Ekspektasi
                         </p>
                         <p class="mb-0">
                             <span class="fw-medium me-1">Potensi</span>
-                            <small class="text-muted">Rendah</small>
+                            <small class="">Rendah</small>
                         </p>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-lg-4 mb-4">
-                <div class="card card-border-shadow-warning">
+                <div class="card card-border-shadow-warning bg-warning text-white">
                     <div class="card-body">
                         <div class="d-flex align-items-center mb-2 pb-1">
                             <div class="avatar me-2">
@@ -101,40 +101,40 @@
                                             class="ti ti-user ti-md"></i></span>
                                 </a>
                             </div>
-                            <h4 class="ms-1 mb-0">5</h4>
+                            <h4 class="ms-1 mb-0 text-white">5</h4>
                         </div>
                         <p class="mb-1">Kinerja Sesuai Ekspektasi
                         </p>
                         <p class="mb-0">
                             <span class="fw-medium me-1">Potensi</span>
-                            <small class="text-muted">Menengah</small>
+                            <small class="">Menengah</small>
                         </p>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-lg-4 mb-4">
-                <div class="card card-border-shadow-info">
+                <div class="card card-border-shadow-success bg-success text-white">
                     <div class="card-body">
                         <div class="d-flex align-items-center mb-2 pb-1">
                             <div class="avatar me-2">
                                 <a href="?page=data_box6">
-                                    <span class="avatar-initial rounded bg-label-info"><i
+                                    <span class="avatar-initial rounded bg-label-success"><i
                                             class="ti ti-user ti-md"></i></span>
                                 </a>
                             </div>
-                            <h4 class="ms-1 mb-0">6</h4>
+                            <h4 class="ms-1 mb-0 text-white">6</h4>
                         </div>
                         <p class="mb-1">Kinerja Sesuai Ekspektasi
                         </p>
                         <p class="mb-0">
                             <span class="fw-medium me-1">Potensi</span>
-                            <small class="text-muted">Tinggi</small>
+                            <small class="">Tinggi</small>
                         </p>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-lg-4 mb-4">
-                <div class="card card-border-shadow-danger">
+                <div class="card card-border-shadow-danger bg-danger text-white">
                     <div class="card-body">
                         <div class="d-flex align-items-center mb-2 pb-1">
                             <div class="avatar me-2">
@@ -143,52 +143,52 @@
                                             class="ti ti-user ti-md"></i></span>
                                 </a>
                             </div>
-                            <h4 class="ms-1 mb-0">1</h4>
+                            <h4 class="ms-1 mb-0 text-white">1</h4>
                         </div>
                         <p class="mb-1">Kinerja Di bawah Ekspektasi</p>
                         <p class="mb-0">
                             <span class="fw-medium me-1">Potensi</span>
-                            <small class="text-muted">Rendah</small>
+                            <small class="">Rendah</small>
                         </p>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-lg-4 mb-4">
-                <div class="card card-border-shadow-warning">
+                <div class="card card-border-shadow-danger bg-danger text-white">
                     <div class="card-body">
                         <div class="d-flex align-items-center mb-2 pb-1">
                             <div class="avatar me-2">
                                 <a href="?page=data_box2">
-                                    <span class="avatar-initial rounded bg-label-warning"><i
+                                    <span class="avatar-initial rounded bg-label-danger"><i
                                             class="ti ti-user ti-md"></i></span>
                                 </a>
                             </div>
-                            <h4 class="ms-1 mb-0">2</h4>
+                            <h4 class="ms-1 mb-0 text-white">2</h4>
                         </div>
                         <p class="mb-1">Kinerja Di bawah Ekspektasi</p>
                         <p class="mb-0">
                             <span class="fw-medium me-1">Potensi</span>
-                            <small class="text-muted">Menengah</small>
+                            <small class="">Menengah</small>
                         </p>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-lg-4 mb-4">
-                <div class="card card-border-shadow-info">
+                <div class="card card-border-shadow-warning bg-warning text-white">
                     <div class="card-body">
                         <div class="d-flex align-items-center mb-2 pb-1">
                             <div class="avatar me-2">
                                 <a href="?page=data_box3">
-                                    <span class="avatar-initial rounded bg-label-info"><i
+                                    <span class="avatar-initial rounded bg-label-warning"><i
                                             class="ti ti-user ti-md"></i></span>
                                 </a>
                             </div>
-                            <h4 class="ms-1 mb-0">3</h4>
+                            <h4 class="ms-1 mb-0 text-white">3</h4>
                         </div>
                         <p class="mb-1">Kinerja Di bawah Ekspektasi</p>
                         <p class="mb-0">
                             <span class="fw-medium me-1">Potensi</span>
-                            <small class="text-muted">Tinggi</small>
+                            <small class="">Tinggi</small>
                         </p>
                     </div>
                 </div>
